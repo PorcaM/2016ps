9bcd1ad14b5faccdcc13b2aee815b20df3910865
PorcaM <tjdwns5122@gmail.com>
2016-04-01 18:02:43 +0900

    mark_success

--- alliance.cpp
+++ alliance.cpp
@@ -1,28 +1,33 @@
+/*
+Problem:	alliance - w5_alliance
+Submitted:	18:00
+Language:	C++
+Time used:	3.44 second
+Memory used:	104595456 bytes
+Writed by PorcaM
+*/
+
 #include <iostream>
 #include <list>
 using namespace std;
 
 int T, N, M;
+bool isGroup[100000];
 typedef pair<int, int> IntPair;
 typedef list<IntPair> PairList;
 typedef list<int> IntList;
 PairList al;
+IntList partnerOf[100000];
 
-int getSize() {
-	int ret = 2;
-	IntList member;
-	member.push_back(al.begin()->first);
-	member.push_back(al.begin()->second);
-	for (IntList::iterator mi = member.begin();
-	mi != member.end(); mi++) {
-		int me = *mi;
-		for (PairList::iterator li = al.begin();
-		li != al.end();) {
-			if (li->first == me || li->second == me) {
-				li = al.erase(li);
-				ret++;
-			}
-			else li++;
+int mark(int num) {
+	if (isGroup[num]) return 0;
+	else isGroup[num] = true;
+	int ret = 1;
+	if (partnerOf[num].size() == 0);
+	else {
+		for (IntList::iterator pi = partnerOf[num].begin();
+		pi != partnerOf[num].end(); pi++) {
+			ret += mark(*pi);
 		}
 	}
 	return ret;
@@ -33,19 +38,29 @@ int main() {
 	cin >> T;
 	while (T--) {
 		cin >> N >> M;
-
+		for (int i = 0; i < N; i++) {
+			partnerOf[i].clear();
+			isGroup[i] = false;
+		}
 		for (int i = 0; i < M; i++) {
 			int a, b;
 			cin >> a >> b;
+			a--; b--;
 			al.push_back(IntPair(a, b));
+			partnerOf[a].push_back(b);
+			partnerOf[b].push_back(a);
 		}
 
-		int max = 0;
-		while (al.size() != 0) {
-			int size = getSize();
-			if (max < size)
-				max = size;
+		IntList marks;
+		for (int i = 0; i < N; i++) {
+			if (isGroup[i]) continue;
+			else marks.push_back(mark(i));
 		}
+		int max = 0;
+		for (IntList::iterator mi = marks.begin();
+		mi != marks.end(); mi++) {
+			if (max < *mi) max = *mi;
+		}		
 		cout << max << "\n";
 	}
 }
