db78876799a9b1446f2183b64e983849c39964cc
PorcaM <tjdwns5122@gmail.com>
2016-05-13 02:42:32 +0900

    w10_success

--- w10_hacking.cpp
+++ w10_hacking.cpp
@@ -1,22 +1,43 @@
+/*
+
+Problem:	Hacking - w10_Hacking
+Submitted:	02:41
+Language:	C++
+Time used:	3.2 second
+Memory used:	61440 bytes
+
+*/
 #include <stdio.h>
 using namespace std;
 
-int T, N, c, i, t; 
-bool bl[50001];
+#define MAX_NUM		50000
+#define MAX_MISS	3
+
+int		T, N, c;
+char	bl[MAX_NUM / 8];
 
 int main() {
 	scanf("%d", &T);
 	while (T--) {
 		scanf("%d %d", &N, &c);
-		for (i = 0; i < N + 1; i++) {
-			bl[i] = false;
-		}
-		for (i = 0; i < N - c; i++) {
+		int ii = N / 8, bi = N % 8;
+		for (int i = 0; i < ii+1; i++) bl[i] = 0xFF;
+		bl[0] &= 0xFE;
+		for (int i = 0; i < N - c; i++) {
+			int t, it, of;
 			scanf("%d", &t);
-			bl[t] = true;
+			it = t / 8; of = t % 8;
+			char ct = 0x01;
+			ct = (ct << of) ^ 0xFF;
+			bl[it] &= ct;
 		}
-		for (i = 1; i < N+1; i++) {
-			if (!bl[i]) printf("%d ", i);
+		for (int i = 0; i < ii+1; i++) {
+			for (int j = 0; j < 8; j++) {
+				if ((bl[i] >> j) & 0x01) {
+					printf("%d ", i * 8 + j);
+				}
+				if (i == ii && j == bi) break;
+			}
 		}
 		printf("\n");
 	}
--- w10_tile.cpp
+++ w10_tile.cpp
@@ -1,53 +1,63 @@
+/*
+
+Problem:	Tile - w10_Tile
+Submitted:	01:54
+Language:	C++
+Time used:	0.07 second
+Memory used:	131072 bytes
+
+*/
 #include <iostream>
-#include <string.h>
 #include <algorithm>
+#include <string.h>
 using namespace std;
 
-struct Tile
-{
-	int x, y;
+#define MAX_NUM  500
+
+struct Tile{
+	int	a, b;
 };
 
-int T, K;
-Tile tl[500];
-int bc[500];
-int lis[500];
+int		T, K;
+Tile	tl[MAX_NUM];
+int		lis[MAX_NUM];
+
+bool cmp(const Tile a, const Tile b) {
+	return a.a < b.a || (a.a == b.a && a.b < b.b);
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin >> T;
 	while (T--) {
-		memset(bc, 0, sizeof(bc));
-		memset(lis, 0, sizeof(lis));
 		cin >> K;
 		for (int i = 0; i < K; i++) {
 			int A, B;
 			cin >> A >> B;
-			if (A < B) swap(A, B);
-			tl[i].x = A; tl[i].y = B;
+			if	(A < B)		swap(A, B);
+			tl[i].a = A; tl[i].b = B;
+			lis[i] = 1;
 		}
-		for (int i = 0; i < K; i++) {
-			Tile base = tl[i];
-			for (int j = 0; j < K; j++) {
-				Tile ti = tl[j];
-				if (ti.x <= base.x && ti.y <= base.y) bc[i]++;
-			}
-		}
-		sort(bc, bc + K);
-		for (int i = 0; i < K; i++) {
-			int max_value = 0;
+		sort(tl, tl + K, cmp);
+		/*cout << "=============test==========\n";
+		for (int i = 0; i < K; i++) cout << tl[i].a << "\t";
+		cout << "\n";
+		for (int i = 0; i < K; i++) cout << tl[i].b << "\t";
+		cout << "\n";*/
+		for (int i = 1; i < K; i++) {
 			for (int j = 0; j < i; j++) {
-				if (max_value < lis[j] && bc[j] < bc[i]) max_value = lis[j];
+				if (tl[i].b >= tl[j].b && lis[i] < lis[j] + 1) {
+					lis[i] = lis[j] + 1;
+				}
 			}
-			lis[i] = max_value + 1;
 		}
-		for (int i = 0; i < K; i++)
-			cout << bc[i] << " ";
-		cout << endl;
-		for (int i = 0; i < K; i++)
-			cout << lis[i] << " ";
-		cout << endl;
-		cout << lis[K - 1] << "\n";
+		/*for (int i = 0; i < K; i++) cout << lis[i] << "\t";
+		cout << "\n";*/
+		int max_value = 0;
+		for (int i = 0; i < K; i++) {
+			if (max_value < lis[i]) max_value = lis[i];
+		}
+		cout << max_value << endl;
 	}
 	return 0;
 }
